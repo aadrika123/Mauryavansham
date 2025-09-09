71b1d9e5e324383f46fdd48d6ac079dd3d0a808c
akshay2s <akshay.aadrika@gmail.com>
2025-09-09 17:08:52 +0530

    changes

---
+++ src/app/admin/ad-rates/page.tsx
@@ -0,0 +1,22 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/src/lib/auth";
+import { redirect } from "next/navigation";
+import AdmindashboardLayout from "@/src/components/layout/adminDashboardLayout";
+import AdRatePage from "./adRatesPage";
+
+export default async function AdminAdsPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user?.id) {
+    redirect("/sign-in");
+  }
+
+  return (
+    <AdmindashboardLayout user={session.user}>
+      <div className="container mx-auto px-4 py-8">
+       
+        <AdRatePage />
+      </div>
+    </AdmindashboardLayout>
+  );
+}
